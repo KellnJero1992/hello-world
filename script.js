01843f8925713231ccb20b56f6844650a68a0d86
KellnJero1992 <142959905+KellnJero1992@users.noreply.github.com>
2023-09-23 18:27:35 -0700

    Update script.js
    change quotes

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
-const images = document.querySelectorAll('#slider img');
-const previousImage = document.getElementById("prev");
-const nextImage = document.getElementById("next");
-let currentIndex = 0;
-function reset() {
-  for (let i = 0; i < images.length; i++) {
-    images[i].classList.remove('active');
+const button = document.getElementById("new-quote-btn");
+const quoteDiv = document.getElementById("quote-output");
+const authorDiv = document.getElementById("author-output");
+const quotes = [
+  {
+    quote: "If you talk to a man in a language he understands, that goes to his head. If you talk to him in his own language, that goes to his heart.",
+    author: "— Nelson Mandela"
+  },
+  {
+    quote: "Learning another language is not only learning different words for the same things, but learning another way to think about things.",
+    author: "— Flora Lewis"
+  },
+  {
+    quote: "Language is the road map of a culture. It tells you where its people come from and where they are going.",
+    author: "— Rita Mae Brown"
+  },
+  {
+    quote: "Language shapes the way we think, and determines what we can think about.",
+    author: "— Benjamin Lee Whorf"
+  },
+  {
+    quote: "You live a new life for every new language you speak. If you know only one language, you live only once.",
+    author: "— Czech Proverb"
+  },
+  {
+    quote: "The most intimate temper of a people, its deepest soul, is above all in its language. ",
+    author: "— Jules Michelet"
   }
-}
-function initializeSlider() {
-  reset();
-  images[currentIndex].classList.add('active');
-}
-function slideLeft() {
-  reset();
-  currentIndex--;
-  if (currentIndex < 0) {
-    currentIndex = images.length - 1;
+]
+button.addEventListener("click", function() {
+  let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+
+  if (quoteDiv.innerText !== randomQuote.quote) {
+    quoteDiv.innerText = randomQuote.quote;
+    authorDiv.innerText = randomQuote.author;
+  } else {
+    randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+    quoteDiv.innerText = randomQuote.quote;
+    authorDiv.innerText = randomQuote.author;
   }
-  images[currentIndex].classList.add('active');
-}
-
-function slideRight() {
-  reset();
-  currentIndex++;
-  if (currentIndex >= images.length) {
-    currentIndex = 0;
-  }
-  images[currentIndex].classList.add('active');
-}
-initializeSlider();
-
-previousImage.addEventListener('click', function() {
-  slideLeft();
-});
-
-nextImage.addEventListener('click', function() {
-  slideRight();
 });
