365ae5e0034dc925be68da8cff6ebc2c3f2d79e6
KellnJero1992 <142959905+KellnJero1992@users.noreply.github.com>
2023-09-30 11:33:19 -0700

    Update script.js
    code updates

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ const quoteDiv = document.getElementById("quote-output");
 const authorDiv = document.getElementById("author-output");
 const quotes = [
   {
+    quote: "A different language is a different vision of life.",
+    author: "— Federico Fellini"
+  },
+  {
+    quote: "The limits of my language mean the limits of my world.",
+    author: "— Ludwig Wittgenstein"
+  },
+  {
+    quote: "Language is the road map of a culture. It tells you where its people come from and where they are going.",
+    author: "— Rita Mae Brown"
+  },
+  {
+    quote: "He who knows no foreign languages knows nothing of his own.",
+    author: "— Confucius"
+  },
+  {
     quote: "If you talk to a man in a language he understands, that goes to his head. If you talk to him in his own language, that goes to his heart.",
     author: "— Nelson Mandela"
   },
@@ -11,21 +27,21 @@ const quotes = [
     author: "— Flora Lewis"
   },
   {
-    quote: "Language is the road map of a culture. It tells you where its people come from and where they are going.",
-    author: "— Rita Mae Brown"
+    quote: "Knowledge of languages is the doorway to wisdom.",
+    author: "— Roger Bacon"
   },
   {
-    quote: "Language shapes the way we think, and determines what we can think about.",
-    author: "— Benjamin Lee Whorf"
+    quote: "Language is the blood of the soul into which thoughts run and out of which they grow.",
+    author: "— Oliver Wendell Holmes"
   },
   {
-    quote: "You live a new life for every new language you speak. If you know only one language, you live only once.",
-    author: "— Czech Proverb"
+    quote: "Language is not a genetic gift, it is a social gift. Learning a new language is becoming a member of the club – the community of speakers of that language.",
+    author: "— Nelson Mandela"
   },
   {
-    quote: "The most intimate temper of a people, its deepest soul, is above all in its language. ",
-    author: "— Jules Michelet"
-  }
+    quote: "A special kind of beauty exists which is born in language, of language, and for language.",
+    author: "— Gaston Bachelard"
+  },
 ]
 button.addEventListener("click", function() {
   let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
